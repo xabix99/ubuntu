04988139de78c2f1aa69e5fa8884fee597c71aed
xabix99 <118305310+xabix99@users.noreply.github.com>
2023-04-17 16:30:00 +0200

    s

--- bashe/aplikacje/installthemes.sh
+++ bashe/aplikacje/installthemes.sh
@@ -15,19 +15,22 @@ sudo apt install -y gnome-shell-extensions
 sudo apt install -y gnome-tweaks
 sudo apt install -y unzip
 sudo apt install -y zip
-gnome-shell-extension-tool -e user-themes
+gnome-extensions enable user-theme
 sudo add-apt-repository ppa:papirus/papirus -y
 cd /usr/share/themes
-wget "https://github.com/dracula/gtk/archive/master.zip"
-unzip master.zip && sudo rm master.zip
+wget https://github.com/dracula/gtk/archive/master.zip
+unzip master.zip && sudo rm -rf master.zip
 # Ustawienia wygląðu
 gsettings set org.gnome.desktop.interface gtk-theme "gtk-master"
 gsettings set org.gnome.desktop.wm.preferences theme "Yaru-purple-dark"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-# Dock na dole na srodku
+# Dock settings
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 1.0
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 printf %"$COLUMNS"s |tr " " "-"
 echo Aktualizowanie wszystkiego
 printf %"$COLUMNS"s |tr " " "-"
