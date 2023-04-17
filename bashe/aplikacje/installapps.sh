11e0baac5a6a3e72408afc298aa4d71c2ad1ece4
XaBix <118305310+xabix99@users.noreply.github.com>
2023-04-17 09:20:24 +0200

    s

---
+++ bashe/aplikacje/installapps.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -eu -o pipefail
+
+sudo -n true
+test $? -eq 0 || exit 1 "potrzebujesz uprawnien sudo aby uzyc tego skryptu"
+
+read -p "Czy twoj system to ubuntu? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+printf %"$COLUMNS"s |tr " " "-"
+echo Aktualizowanie systemu
+printf %"$COLUMNS"s |tr " " "-"
+sudo apt-get update -y 
+
+printf %"$COLUMNS"s |tr " " "-"
+echo Instalowanie aplikacji
+printf %"$COLUMNS"s |tr " " "-"
+sudo apt install -y git
+sudo apt install -y zsh
+sudo apt install -y curl
+sudo apt install -y dconf
+sudo apt install -y zip
+sudo apt install -y unzip
+sudo apt install -y wget
+sudo apt install -y snapd
+sudo apt install -y discord
+# Brave Browser Install
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install brave-browser -y
+sudo apt install -y gnome-shell-extensions
+sudo apt install -y gnome-tweaks
+# Programowanie
+sudo apt install -y nodejs
+sudo apt install -y npm
+sudo apt install -y gcc
+# Visual Studio Code
+sudo apt install software-properties-common apt-transport-https wget -y
+sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update -y && sudo apt install code
+# Github Desktop
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+sudo apt update -y && sudo apt install github-desktop
+printf %"$COLUMNS"s |tr " " "-"
+echo Aktualizowanie pakietow
+printf %"$COLUMNS"s |tr " " "-"
+sudo apt update
+sudo apt install --fix-missing -y
+sudo apt upgrade --allow-downgrades -y
+sudo apt full-upgrade --allow-downgrades -y
+printf %"$COLUMNS"s |tr " " "-"
+echo Finalizowanie instalacji
+printf %"$COLUMNS"s |tr " " "-"
+sudo apt install -f
+sudo apt autoremove -y
+sudo apt autoclean
+sudo apt clean
---
+++ bashe/aplikacje/installthemes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eu -o pipefail
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+
+read -p "Czy twoj system to ubuntu? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+echo Zmiana wygladu
+sudo add-apt-repository ppa:papirus/papirus -y
+gnome-shell-extension-tool -e user-themes
+sudo cd /usr/share/themes
+&& wget "https://github.com/dracula/gtk/archive/master.zip"
+&& unzip gtk-master.zip && sudo rm gtk-master.zip
+gsettings set org.gnome.desktop.interface gtk-theme "gtk-master"
+gsettings set org.gnome.desktop.wm.preferences theme "Yaru-purple-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+echo Aktualizowanie wszystkiego
+sudo apt update
+sudo apt install --fix-missing -y
+sudo apt upgrade --allow-downgrades -y
+sudo apt full-upgrade --allow-downgrades -y
+echo Czyszczenie systemu
+sudo apt install -f
+sudo apt autoremove -y
+sudo apt autoclean
+sudo apt clean
+echo Wszystko gotowe
